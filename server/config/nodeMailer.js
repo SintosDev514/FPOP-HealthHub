8f107d6bb6f94d0286b7b5f8701b1e242f585016
Roberto Sintos <sintosroberto3@gmail.com>
2026-07-13 16:13:40 +0800

    fix: force IPv4 and use port 587 for Gmail SMTP on Render

--- server/config/nodeMailer.js
+++ server/config/nodeMailer.js
@@ -1,11 +1,19 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
+  connectionTimeout: 10000,
+  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4,
 });
 
 export default transporter;
