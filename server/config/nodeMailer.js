dbb15230edd7514576c09757c2f61104049733d9
Roberto Sintos <sintosroberto3@gmail.com>
2026-07-13 17:13:37 +0800

    fix: use accessToken param for EmailJS private key (per API docs)

--- server/config/nodeMailer.js
+++ server/config/nodeMailer.js
@@ -6,6 +6,7 @@ const mailer = {
       service_id: process.env.EMAILJS_SERVICE_ID,
       template_id: process.env.EMAILJS_TEMPLATE_ID,
       user_id: process.env.EMAILJS_PUBLIC_KEY,
+      accessToken: process.env.EMAILJS_PRIVATE_KEY,
       template_params: {
         to_email: to,
         subject,
@@ -15,10 +16,7 @@ const mailer = {
 
     const res = await fetch(EMAILJS_API, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.EMAILJS_PRIVATE_KEY}`,
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(params),
     });
 
